201f8d2fb89e1b3ef435f1c817cad0e25201ac0c
Cole Joffray <colej@MacBook-Pro.lan>
2023-11-08 19:46:51 -0500

    generated gameboard

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
+//Initialize the game board on page load
+initBoard()
+
+function initBoard(){
+  let board = document.getElementById('clue-board')
+  //generate 5 rows, then place 6 boxes in each row
+  for(let i = 0; i < 5; i++){
+    let row = document.createElement('div')
+    let boxValue = 200 * (i + 1)
+    row.className = 'clue-row'
+
+    for(let j = 0; j < 6; j++){
+      let box = document.createElement('div')
+      box.className= 'clue-box'
+      box.textContent = `$${boxValue}`
+      box.addEventListener('click', getClue, false)
+      row.appendChild(box)
+    }
+
+
+    board.appendChild(row)
+
+  }
+}
+
+ function getClue() {
+  console.log('hello world')
+}
+
+
+
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+// document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
